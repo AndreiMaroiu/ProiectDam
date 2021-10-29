1811165c6b129eed4d06456ae96bf59c834d5685
Andrei Maroiu <achimaroiu@yahoo.com>
2021-10-29 19:23:35 +0300

    Updated CameraFollow
    Implemented smoothing for camera follow.

--- ProiectDam/Assets/Scripts/Gameplay/Player/CameraFollow.cs
+++ ProiectDam/Assets/Scripts/Gameplay/Player/CameraFollow.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using Gameplay.Events;
 
 namespace Gameplay.Player
 {
     public class CameraFollow : MonoBehaviour
     {
-        [SerializeField] private PlayerController _playerController;
+        [SerializeField] private RoomBehaviourEvent _roomEvent;
+        [SerializeField] private float _smoothTime = 1;
+
+        private Vector3 _velocity = Vector3.zero;
 
         private void LateUpdate()
         {
-            Vector3 target = _playerController.transform.position;
-            transform.position = new Vector3(target.x, target.y, transform.position.z);
+            Vector3 position = _roomEvent.Value.transform.position;
+            Vector3 target = new Vector3(position.x, position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref _velocity, _smoothTime); ;
         }
     }
 }
