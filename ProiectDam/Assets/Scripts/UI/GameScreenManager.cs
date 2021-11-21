7c2a9c01f69ab6a824c258325a701e8b482de515
Marius Gociman <mariusgociman74@yahoo.it>
2021-11-22 00:01:14 +0200

    Added layer preview feature

--- ProiectDam/Assets/Scripts/UI/GameScreenManager.cs
+++ ProiectDam/Assets/Scripts/UI/GameScreenManager.cs
@@ -12,17 +12,28 @@ namespace UI
         [SerializeField] private IntEvent _layersCountEvent;
         [SerializeField] private IntEvent _currentLayerEvent;
 
+        [SerializeField] private BoolEvent _previewActive;
+        [SerializeField] private Text _previewText;
+
+        public float LastTimeScale { get; set; }
+
+        public const float StoppedScale = 0.0f;
+
         private void Start()
         {
             _layersCountEvent.OnValueChanged += OnLayersCountChanged;
             _layerSlider.onValueChanged.AddListener(OnSliderChanged);
+
+            _previewActive.OnValueChanged += OnPreviewChanged;
+
+            _layerSlider.gameObject.SetActive(false);
+            LastTimeScale = Time.timeScale;
 
             OnLayersCountChanged();
         }
 
         private void OnLayersCountChanged()
         {
-            _layerSlider.gameObject.SetActive(_layersCountEvent.Value > 1);
             _layerSlider.maxValue = _layersCountEvent.Value - 1;
             _layerSlider.value = _currentLayerEvent.Value;
         }
@@ -33,10 +44,32 @@ namespace UI
             _layerSlider.value = Mathf.RoundToInt(value);
         }
 
+        private void OnPreviewChanged()
+        {
+            if (!_previewActive.Value)
+            {
+                Time.timeScale = StoppedScale;
+                _layerSlider.gameObject.SetActive(_layersCountEvent.Value > 1);
+                _previewText.text = "ON";
+            }
+            else
+            { 
+                _layerSlider.gameObject.SetActive(false);
+                _previewText.text = "OFF";
+                Time.timeScale = LastTimeScale;
+            }
+        }
+
+        public void OnPreviewClick()
+        {
+            _previewActive.Value = !_previewActive.Value;
+        }
+
         private void OnDestroy()
         {
             _layersCountEvent.OnValueChanged -= OnLayersCountChanged;
             _layerSlider.onValueChanged.RemoveListener(OnSliderChanged);
+            _previewActive.OnValueChanged -= OnPreviewChanged;
         }
     }
 }
