4054dd66dc00ed954c9b0aea1df7a5260caf531d
Iancu Norbert <iancu.norbert123@gmail.com>
2021-12-04 15:40:35 +0200

    Configured monsters' animations
    After so many days, I finally did it. Now, all the monsters (in the playable game) have functional animations.

--- ProiectDam/Assets/Scripts/Gameplay/Enemies/SimpleEnemy.cs
+++ ProiectDam/Assets/Scripts/Gameplay/Enemies/SimpleEnemy.cs
@@ -6,10 +6,18 @@ namespace Gameplay.Enemies
     public sealed class SimpleEnemy : BaseEnemy
     {
         private EnemySoundHandler _soundhandler;
+        private const string WALK_ANIMATION = "Walk";
+        private const string MELEE_ANIMATION = "Melee";
+        private const string DEATH_ANIMATION = "Death";
+        private const string HIT_ANIMATION = "Hit";
+        private Animator _animator;
+        private SpriteRenderer _renderer;
 
         private void Start()
         {
+            _animator = GetComponent<Animator>();
             _soundhandler = GetComponent<EnemySoundHandler>();
+            _renderer = GetComponent<SpriteRenderer>();
             MaxHealth = _startHealth;
             Health = _startHealth;
 
@@ -20,26 +28,42 @@ namespace Gameplay.Enemies
         {
             // play sound and animations
             _soundhandler.PlayHit();
+            _animator.SetBool(HIT_ANIMATION, true);
+        }
+
+        private void OnStopDamage()
+        {
+            // play sound and animations
+            _animator.SetBool(HIT_ANIMATION, false);
         }
 
         protected override void OnDeath()
         {
-            
+            _animator.SetBool(DEATH_ANIMATION, true);
         }
 
         public override void OnAttack()
         {
             // play sounds and animations
+            _animator.SetBool(MELEE_ANIMATION, true);
+        }
+        public  void OnStopAttack()
+        {
+            // play sounds and animations
+            _animator.SetBool(MELEE_ANIMATION, false);
         }
 
         protected override void OnMove(Vector2Int direction)
         {
             //play sounds and animations
+            _renderer.flipX = direction.x < 0;
+            _animator.SetBool(WALK_ANIMATION, true);
         }
 
         protected override void OnStopMoving()
         {
-            _soundhandler.Stop();
+           // _soundhandler.Stop();
+            _animator.SetBool(WALK_ANIMATION, false);
         }
 
         protected override bool CanMoveToTile(TileType tile)
