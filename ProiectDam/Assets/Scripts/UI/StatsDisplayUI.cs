3b1c3cdcf8b960812b322269cdb860525f0ef402
Marius Gociman <mariusgociman74@yahoo.it>
2022-07-23 17:02:09 +0300

    Added Statistics UI

--- ProiectDam/Assets/Scripts/UI/StatsDisplayUI.cs
+++ ProiectDam/Assets/Scripts/UI/StatsDisplayUI.cs
@@ -7,12 +7,18 @@ namespace UI
     public class StatsDisplayUI : MonoBehaviour
     {
         [SerializeField] private Text _runsText;
+        [SerializeField] private Text _winsText;
+        [SerializeField] private Text _lossesText;
+        [SerializeField] private Text _highText;
 
         private void Start()
         {
             Statistics stats = StatisticsManager.Instance.LoadStats();
 
             _runsText.text = stats.TotalRuns.ToString();
+            _winsText.text = stats.Wins.ToString();
+            _lossesText.text = stats.Losses.ToString();
+            _highText.text = stats.Highscore.ToString();
         }
     }
 }
