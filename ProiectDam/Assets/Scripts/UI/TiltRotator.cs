82021b0e61a54e252ea6a23509f2545b9beea9e5
Andrei Maroiu <achimaroiu@yahoo.com>
2023-01-22 12:47:30 +0200

    updated tilt rotator

--- ProiectDam/Assets/Scripts/UI/TiltRotator.cs
+++ ProiectDam/Assets/Scripts/UI/TiltRotator.cs
@@ -5,19 +5,16 @@ using System;
 
 namespace UI
 {
-    [RequireComponent(typeof(RectTransform))]
     public class TiltRotator : MonoBehaviour
     {
         [SerializeField] private float _limit;
         [SerializeField] private float _speed;
 
-        private RectTransform _rect;
-        private Vector2 _startPos;
+        private Vector3 _startPos;
 
         private void Start()
         {
-            _rect = GetComponent<RectTransform>();
-            _startPos = _rect.anchoredPosition;
+            _startPos = transform.position;
 
             Input.gyro.enabled = true;
         }
@@ -27,13 +24,13 @@ namespace UI
         private void Update()
         {
             Vector3 tilt = Input.gyro.rotationRate;
-            Vector2 temp = new(tilt.y, tilt.x);
-            Vector2 target = _rect.anchoredPosition + temp * Time.deltaTime * _speed;
+            Vector3 temp = new(tilt.y, tilt.x);
+            Vector3 target = transform.position + _speed * Time.deltaTime * temp;
 
             target.x = Math.Clamp(target.x, _startPos.x - _limit, _startPos.x + _limit);
             target.y = Math.Clamp(target.y, _startPos.y - _limit, _startPos.y + _limit);
 
-            _rect.anchoredPosition = target;
+            transform.position = target;
         }
 
 //#endif
